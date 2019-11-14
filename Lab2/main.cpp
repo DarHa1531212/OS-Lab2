89bd4f04a08b6b5870f9b4678853a6aa354aa550
marc <map555@icloud.com>
2019-11-14 10:35:51 -0500

    Générer vecteur.

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -10,6 +10,27 @@ int compositionObjets[5][5] = {{2, 1, 0, 0, 0},
 							   {0, 0, 0, 4, 4},
 							   {2, 0, 0, 0, 1}};
 
+
+//Génère le vecteur aléatoire de demande de créatio des objes et le retourne
+vector<int> GenererVecteurDemandeCreationObjet()
+{
+
+    vector<int> DemandeCreationObjet vector<int>(5);
+
+    //loop pour remplir le vecteur
+    for(int x=0; x<5; x++)
+    {
+        int y=rand(0,6);
+        DemandeCreationObjet.at(x)=y;
+
+
+    }
+
+    return DemandeCreationObjet;
+
+}
+
+
 /**
 * @brief  Calcul le maximum d'objet d'un seul type que l'on peut creer avec les ressources
 * dans le systeme
@@ -109,6 +130,7 @@ void GenererA()
 	}
 }
 
+//Calcule case par case la différence entre demandeMaximal et allocation
 void CalculerCmoinsA(int matriceTemporairePourCmoinsA[5][5])
 {
 
@@ -123,6 +145,7 @@ void CalculerCmoinsA(int matriceTemporairePourCmoinsA[5][5])
 	}
 }
 
+//La somme de chaque colonne de la matrice allocation
 void CalculerV(int matriceTemporairePourV[5])
 {
 
