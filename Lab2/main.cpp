32babc3663e0f68ef6aca3e3dcea9e9d2f802b3d
marc <map555@icloud.com>
2019-11-14 09:32:33 -0500

    Ajout de la fonction permettant de calculer la différence entre C et A ainsi que la fonction pour le calcul de V. :D

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int ressourcesTotal[5] = {12, 20, 14, 8, 16};
-int C[5][5];
+int demandeMaximale[5][5];
+int allocation[5][5];
 
 void CalculerC() {
 
-     
+
 
 
 
@@ -24,10 +25,44 @@ void GenererA() {
 
 
 
-void CalculerCMoinsA() {
+void CalculerCmoinsA(int matriceTemporairePourCmoinsA[5][5])
+{
+
+    for (int x = 0; x < 5; x++)
+    {
+
+        for (int y = 0; y < 5; y++)
+        {
+
+            matriceTemporairePourCmoinsA[x][y] = (demandeMaximale[x][y] - allocation[x][y]);
+
+        }
+
+    }
+
+}
 
 
+void CalculerV(int matriceTemporairePourV[5])
+{
 
+
+    for (int x = 0; x < 5; x++)
+    {
+
+        int sommeColonneMatriceAllocation=0;
+
+        for (int y = 0; y < 5; y++)
+        {
+
+            sommeColonneMatriceAllocation+=allocation[y][x];
+
+        }
+
+        matriceTemporairePourV[x]=ressourcesTotal[x]-sommeColonneMatriceAllocation;
+
+
+    }
 
 
 }
